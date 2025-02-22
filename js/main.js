5ac7100a8675857d59a75e20d96975e5d3770f91
Tatevik777 <tatevik777@bk.ru>
2025-02-22 21:20:37 +0300

    update

--- js/main.js
+++ js/main.js
@@ -19,3 +19,27 @@ function checkAuth() {
 const start = startOnce(checkAuth);
 
 start();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const iconWrappers = document.querySelectorAll('.icon-wrapper');
+    iconWrappers.forEach(wrapper => {
+        wrapper.addEventListener('click', function () {
+            const url = this.getAttribute('data-url');
+            if (url) {
+                window.location.href = url;
+            }
+        });
+    });
+    renderTargets() 
+});
+
+function renderTargets() {
+    let targets = JSON.parse(localStorage.getItem("targets")) || [];
+
+    targets.forEach(target => {
+    // Проверяем, нет ли уже такой цели в контейнере
+        if (!document.querySelector(`[data-id="${target.id}"]`)) {
+            addTargetToDOM(target);
+        }
+    });
+}
