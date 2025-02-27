6799331f05b5e3027685a115c1452df0f1c6a220
Tatevik777 <tatevik777@bk.ru>
2025-02-27 10:19:19 +0300

    main_update

--- js/authorization.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("authorizationBtn").addEventListener("click", function () {
-    localStorage.setItem("buttonClicked", true);
-    window.location.href = "./index.html";
-    console.log("hello");
-});
